897ae627b50be5c0536d8ce31af5d20de003312f
Szymon Łabuz <szimin18@gmail.com>
2016-11-07 17:46:09 +0100

    A star API

--- src/main/scala/astarlib/AStar.scala
+++ src/main/scala/astarlib/AStar.scala
@@ -1,7 +1,29 @@
 package astarlib
 
-trait AStarNode[T] {
-  def neighbours: List[T]
+case class AStarNode[T](name: String, data: T)
+
+trait AStarAPI[T] {
+
+  def start: AStarNode[T]
+
+  def end: AStarNode[T]
+
+  def neighbours(node: T): List[T]
+
   def heuristic(node1: T, node2: T): Double
 
+  def cost(node1: T, node2: T): Double
+}
+
+object AStarAlgorithm {
+
+  def solve[T](aStarAPI: AStarAPI[T]): List[AStarNode[T]] = {
+
+
+
+
+
+
+    ???
+  }
 }
