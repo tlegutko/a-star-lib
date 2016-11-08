792d577f3b50a68a48cd7d177f664e24aa7fb62b
tlegutko <tom.legutko@gmail.com>
2016-11-08 13:59:22 +0100

    Additional challenging test.

--- src/test/scala/astarlib/AStar2DimensionsSpec.scala
+++ src/test/scala/astarlib/AStar2DimensionsSpec.scala
@@ -12,6 +12,18 @@ class AStar2DimensionsSpec extends FlatSpec with Matchers {
     val correctPath = List(Point(0, 0), Point(1, 1))
     AStar2Dimensions(map).solve() should be(correctPath)
   }
+  "a-star-lib" should "return correct value for more challenging case" in {
+    val map = Seq(
+      Seq(1, 0, 0, 1, 0),
+      Seq(1, 0, 1, 0, 1),
+      Seq(1, 0, 1, 0, 1),
+      Seq(1, 0, 1, 0, 1),
+      Seq(0, 1, 0, 0, 1)
+    )
+    val correctPath = List(Point(0, 0), Point(0, 1), Point(0, 2), Point(0, 3), Point(1, 4), Point(2, 3),
+      Point(2, 2), Point(2, 1), Point(3, 0), Point(4, 1), Point(4, 2), Point(4, 3), Point(4, 4))
+    AStar2Dimensions(map).solve() should be(correctPath)
+  }
   "a-star-lib" should "return empty list when there is no solution" in {
     val map = Seq(
       Seq(1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
