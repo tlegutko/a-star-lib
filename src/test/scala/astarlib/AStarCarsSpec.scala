ff84f9b3ef5f53d24995a9155d6cd079e60d9647
tlegutko <tom.legutko@gmail.com>
2016-12-19 09:35:58 +0100

    Moved test files to this repo.

--- src/test/scala/astarlib/AStarCarsSpec.scala
+++ src/test/scala/astarlib/AStarCarsSpec.scala
@@ -5,27 +5,27 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class AStarCarsSpec extends FlatSpec with Matchers {
   "a-star-lib" should "return correct value for cars example 01" in {
-    AStarCarsFromFile("../testRepo/text_in/test01.in").solve.length - 1 should be(2)
+    AStarCarsFromFile("src/test/resources/test01.in").solve.length - 1 should be(2)
   }
   "a-star-lib" should "return correct value for cars example 02" in {
-    AStarCarsFromFile("../testRepo/text_in/test02.in").solve.length - 1 should be(3)
+    AStarCarsFromFile("src/test/resources/test02.in").solve.length - 1 should be(3)
   }
   "a-star-lib" should "return correct value for cars example 03" in {
-    AStarCarsFromFile("../testRepo/text_in/test03.in").solve.length - 1 should be(8)
+    AStarCarsFromFile("src/test/resources/test03.in").solve.length - 1 should be(8)
   }
   "a-star-lib" should "return correct value for cars example 04" in {
-    AStarCarsFromFile("../testRepo/text_in/test04.in").solve.length - 1 should be(23)
+    AStarCarsFromFile("src/test/resources/test04.in").solve.length - 1 should be(23)
   }
   "a-star-lib" should "return correct value for cars example 05" in {
-    AStarCarsFromFile("../testRepo/text_in/test05.in").solve.length - 1 should be(24)
+    AStarCarsFromFile("src/test/resources/test05.in").solve.length - 1 should be(24)
   }
   "a-star-lib" should "return correct value for cars example 06" in {
-    AStarCarsFromFile("../testRepo/text_in/test06.in").solve.length - 1 should be(237)
+    AStarCarsFromFile("src/test/resources/test06.in").solve.length - 1 should be(237)
   }
   "a-star-lib" should "return correct value for cars example 07" in {
-    AStarCarsFromFile("../testRepo/text_in/test07.in").solve.length - 1 should be(21)
+    AStarCarsFromFile("src/test/resources/test07.in").solve.length - 1 should be(21)
   }
   "a-star-lib" should "return correct value for cars example 1000x1000" in {
-    AStarCarsFromFile("../testRepo/text_in/test1000x1000.in").solve.length - 1 should be(337)
+    AStarCarsFromFile("src/test/resources/test1000x1000.in").solve.length - 1 should be(337)
   }
 }
