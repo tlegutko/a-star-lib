a5eb08578c00b5c26100d267c1c5da04c7494b57
tlegutko <tom.legutko@gmail.com>
2017-01-21 03:06:29 +0100

    Implemented map with futures and actors.

--- src/main/scala/astarlib/ParallelExecutor.scala
+++ src/main/scala/astarlib/ParallelExecutor.scala
@@ -1,22 +1,41 @@
 package astarlib
 
-import java.util.concurrent.ForkJoinPool
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.pattern.ask
+import akka.util.Timeout
 
-import scala.collection.parallel.{ForkJoinTaskSupport, ParSeq}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
-case class ParallelExecutor(numOfProc: Int) {
-  val taskSupport = new ForkJoinTaskSupport(new ForkJoinPool(numOfProc))
+object ParallelExecutor {
+  val timeout = Timeout(1 day)
   def mapWithSingleThread[A, B](seq: Seq[A], f: A => B): Seq[B] = {
     seq.map(f)
   }
-  def mapParallelCollection[A, B](ps: ParSeq[A], f: A => B): ParSeq[B] = {
-    ps.tasksupport = taskSupport
-    ps.map(f)
-  }
   def mapWithParallelCollections[A, B](s: Seq[A], f: A => B): Seq[B] = {
-    mapParallelCollection(s.par, f).seq
+    s.par.map(f).seq // ideally use par. collection all the time so no copying/converting is needed
   }
-  def mapWithActors[A, B](s: Seq[A], f: A => B): Seq[B] = {
-    ???
+  def mapWithFutures[A, B](s: Seq[A], f: A => B): Seq[B] = {
+    Await.result(Future.traverse(s)(x => Future(f(x))), timeout.duration)
+  }
+}
+
+class ParallelActorsExecutor[A, B](numOfActors: Int) {
+  implicit val timeout: Timeout = ParallelExecutor.timeout
+  private val actorSystem = ActorSystem("DoNotUseMe")
+  private val actors = (0 until numOfActors).map(i =>
+    actorSystem.actorOf(Props[SuperSmartActor[A,B]], i.toString))
+
+  def mapUsingActors(s: Seq[A], f: A => B): Seq[B] = {
+    Await.result(Future.traverse(s.zipWithIndex)({
+      case (x, i) => actors(i % numOfActors) ? (x, f)
+    }), timeout.duration).asInstanceOf[Seq[B]]
+  }
+}
+
+class SuperSmartActor[A,B] extends Actor {
+  override def receive = {
+    case (x: A, f: (A => B)) => sender ! f(x)
   }
 }
--- src/test/scala/astarlib/ParallelExecutorBenchmarksSpec.scala
+++ src/test/scala/astarlib/ParallelExecutorBenchmarksSpec.scala
@@ -2,23 +2,25 @@ package astarlib
 
 import org.scalatest.FunSuite
 
+import scala.util.Random
+
 class ParallelExecutorBenchmarksSpec extends FunSuite {
-  def time[A](f: => A): Double = {
+  def time[A](desc: String, f: => A): Double = {
     val start = System.nanoTime
     f
-    (System.nanoTime() - start) / 1e9
-  }
-  def printScore[A](desc: String, duration: Double): Unit = {
+    val duration = (System.nanoTime() - start) / 1e9
     println(f"$desc in $duration%.3fs")
+    duration
   }
   test("let's test how parallel map implementations perform") {
-    val executor = ParallelExecutor(8)
-    val list = (1 to 4000).toList
-    val f = (_: Int) => (1 to 100000).toList.sum
-    val singleThreadTime = time(executor.mapWithSingleThread(list, f))
-    val parallelCollTime = time(executor.mapWithParallelCollections(list, f))
-    printScore("single thread", singleThreadTime)
-    printScore("parallel", parallelCollTime)
-    assert(singleThreadTime > parallelCollTime)
+    val list = (1 to 1000).toList
+    val f = (_: Int) => (1 to 30000).map(Random.nextInt).sorted.sum
+    val singleThreadTime = time("single thread", ParallelExecutor.mapWithSingleThread(list, f))
+    val parallelCollTime = time("parallel collections", ParallelExecutor.mapWithParallelCollections(list, f))
+    val futuresTime = time("futures", ParallelExecutor.mapWithFutures(list, f))
+    val actorsTime = time("actors", new ParallelActorsExecutor(8).mapUsingActors(list, f))
+    assert(parallelCollTime < singleThreadTime)
+    assert(futuresTime < singleThreadTime)
+    assert(actorsTime < singleThreadTime)
   }
 }
